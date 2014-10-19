81deaa27dcec2cd1f1099dafe7467663466ecbd0
Mike van Dongen <github@mikevandongen.nl>
2014-10-19 18:48:29 +0200

    Added a few complex tests.

--- tests/test.c
+++ tests/test.c
@@ -4,7 +4,7 @@
 
 #include "noexception.h"
 
-#define START_TEST2(name) START_TEST(name) { unsigned int assert_in_order_counter = 0;
+#define START_TEST2(__testname) START_TEST(__testname) { unsigned int assert_in_order_counter = 0;
 #define END_TEST2 } END_TEST
 
 #define assert_in_order(n) (assert_in_order_counter++ == n) ? \
@@ -300,10 +300,209 @@ START_TEST2(test_try_throw_catchall_catch)
 }
 END_TEST2
 
-/*
-Try [throw] [catch | catchall]* [throw] [finally]
-Nested
- */
+START_TEST2(test_try_throw_catch_throw_finally)
+{
+    assert_in_order(0);
+    TRY
+    {
+        assert_in_order(1);
+        THROW(ALPHA_EXCEPTION, "Throwing an Alpha Exception!");
+    }
+    CATCH(ALPHA_EXCEPTION)
+    {
+        assert_in_order(2);
+        THROW(BETA_EXCEPTION, "Throwing a Beta Exception!");
+    }
+    FINALLY
+    {
+        assert_in_order(3);
+    }
+    ETRY;
+    fail("This should never be reached!");
+}
+END_TEST2
+
+START_TEST2(test_try_try_throw_catch_catch)
+{
+    assert_in_order(0);
+    TRY
+    {
+        assert_in_order(1);
+        TRY
+        {
+            assert_in_order(2);
+            THROW(ALPHA_EXCEPTION, "Throwing an Alpha Exception!");
+        }
+        CATCH(ALPHA_EXCEPTION)
+        {
+            assert_in_order(3);
+        }
+        ETRY;
+        assert_in_order(4);
+    }
+    CATCH(ALPHA_EXCEPTION)
+    {
+    }
+    ETRY;
+    assert_in_order(5);
+}
+END_TEST2
+
+START_TEST2(test_try_throw_catch_try_throw_catch_finally_finally)
+{
+    assert_in_order(0);
+    TRY
+    {
+        assert_in_order(1);
+        THROW(ALPHA_EXCEPTION, "Throwing an Alpha Exception!");
+    }
+    CATCH(ALPHA_EXCEPTION)
+    {
+        assert_in_order(2);
+        TRY
+        {
+            assert_in_order(3);
+            THROW(ALPHA_EXCEPTION, "Throwing an Alpha Exception!");
+        }
+        CATCH(ALPHA_EXCEPTION)
+        {
+            assert_in_order(4);
+        }
+        FINALLY
+        {
+            assert_in_order(5);
+        }
+        ETRY;
+        assert_in_order(6);
+    }
+    FINALLY
+    {
+        assert_in_order(7);
+    }
+    ETRY;
+    assert_in_order(8);
+}
+END_TEST2
+
+START_TEST2(test_try_throw_catch_try_throw_catch)
+{
+    assert_in_order(0);
+    TRY
+    {
+        assert_in_order(1);
+        THROW(ALPHA_EXCEPTION, "Throwing an Alpha Exception!");
+    }
+    CATCH(ALPHA_EXCEPTION)
+    {
+        assert_in_order(2);
+    }
+    ETRY;
+
+    assert_in_order(3);
+
+    TRY
+    {
+        assert_in_order(4);
+        THROW(ALPHA_EXCEPTION, "Throwing an Alpha Exception!");
+    }
+    CATCH(ALPHA_EXCEPTION)
+    {
+        assert_in_order(5);
+    }
+    ETRY;
+    assert_in_order(6);
+}
+END_TEST2
+
+START_TEST2(test_try_throw_catch_compare_exceptions)
+{
+    const char * const message = "Throwing an Alpha Exception!";
+    assert_in_order(0);
+    TRY
+    {
+        assert_in_order(1);
+        THROW(ALPHA_EXCEPTION, message);
+    }
+    CATCH(ALPHA_EXCEPTION)
+    {
+        assert_in_order(2);
+        ck_assert_int_eq(e->type, ALPHA_EXCEPTION);
+        ck_assert_str_eq(e->msg, message);
+    }
+    ETRY;
+    assert_in_order(3);
+}
+END_TEST2
+
+static void test_try_call_catch_finally_helper(const char *message)
+{
+    THROW(ALPHA_EXCEPTION, message);
+}
+
+START_TEST2(test_try_call_catch_finally)
+{
+    const char *message = "Throwing an Alpha Exception!";
+    assert_in_order(0);
+    TRY
+    {
+        assert_in_order(1);
+        test_try_call_catch_finally_helper(message);
+        fail("This should never be reached!");
+    }
+    CATCH(ALPHA_EXCEPTION)
+    {
+        assert_in_order(2);
+        ck_assert_str_eq(e->msg, message);
+    }
+    FINALLY
+    {
+        assert_in_order(3);
+    }
+    ETRY;
+    assert_in_order(4);
+}
+END_TEST2
+
+static void test_try_call_catch_catchall_finally_helper(const char *message)
+{
+    TRY
+    {
+        THROW(BETA_EXCEPTION, "Throwing a Beta Exception!");
+    }
+    CATCH(BETA_EXCEPTION)
+    {
+        THROW(CHARLIE_EXCEPTION, message);
+    }
+    ETRY;
+}
+
+START_TEST2(test_try_call_catch_catchall_finally)
+{
+    const char *message = "Throwing a Charlie Exception!";
+    assert_in_order(0);
+    TRY
+    {
+        assert_in_order(1);
+        test_try_call_catch_catchall_finally_helper(message);
+        fail("This should never be reached!");
+    }
+    CATCH(CHARLIE_EXCEPTION)
+    {
+        assert_in_order(2);
+        ck_assert_str_eq(e->msg, message);
+    }
+    CATCHALL
+    {
+        fail("This should never be reached! %d %s", e->type, e->msg);
+    }
+    FINALLY
+    {
+        assert_in_order(3);
+    }
+    ETRY;
+    assert_in_order(4);
+}
+END_TEST2
 
 Suite * regression_test_suite(void)
 {
@@ -330,6 +529,13 @@ Suite * regression_test_suite(void)
     tcase_add_test(tc_core, test_try_throw_catchall_finally);
     tcase_add_test(tc_core, test_try_throw_catch_catchall);
     tcase_add_test(tc_core, test_try_throw_catchall_catch);
+    tcase_add_exit_test(tc_core, test_try_throw_catch_throw_finally, EXIT_FAILURE);
+    tcase_add_test(tc_core, test_try_try_throw_catch_catch);
+    tcase_add_test(tc_core, test_try_throw_catch_try_throw_catch_finally_finally);
+    tcase_add_test(tc_core, test_try_throw_catch_try_throw_catch);
+    tcase_add_test(tc_core, test_try_throw_catch_compare_exceptions);
+    tcase_add_test(tc_core, test_try_call_catch_finally);
+    tcase_add_test(tc_core, test_try_call_catch_catchall_finally);
 
     suite_add_tcase(s, tc_core);
 
@@ -339,107 +545,11 @@ Suite * regression_test_suite(void)
 int main(void)
 {
     int number_failed;
-    Suite *s;
-    SRunner *sr;
-
-    s = regression_test_suite();
-    sr = srunner_create(s);
+    Suite *s = regression_test_suite();
+    SRunner *sr = srunner_create(s);
 
     srunner_run_all(sr, CK_VERBOSE);
     number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
-    return number_failed != 0;
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
-
-
-
-
-int main_2()
-{
-    TRY
-    {
-        printf("trying..\n");
-        THROW(BETA_EXCEPTION, "B!");
-    }
-    CATCH(BETA_EXCEPTION)
-    {
-        printf("catching BETA_EXCEPTION: %s\n", e->msg);
-        TRY
-        {
-            printf("trying..\n");
-            THROW(CHARLIE_EXCEPTION, "C!");
-            printf("won't reach this!\n");
-        }
-        CATCH(CHARLIE_EXCEPTION)
-        {
-            printf("catching C_EXCEPTION: %s\n", e->msg);
-        }
-        FINALLY
-        {
-            printf("finally\n");
-        }
-        ETRY;
-        printf("reach this!\n");
-    }
-    FINALLY
-    {
-        printf("cleaning up!\n");
-    }
-    ETRY;
-
-    printf("\n");
-
-    TRY
-    {
-        printf("trying..\n");
-        /*tc_sub1();*/
-    }
-    CATCH(ALPHA_EXCEPTION)
-    {
-        printf("catching ALPHA_EXCEPTION: %s (%s)\n", e->msg, e->previous ? e->previous->msg : "no rethrow");
-    }
-    CATCH(BETA_EXCEPTION)
-    {
-        printf("catching BETA_EXCEPTION..\n");
-    }
-    FINALLY
-    {
-        printf("cleaning up!\n");
-    }
-    ETRY;
-
-    printf("\n");
-
-    TRY
-    {
-        printf("trying..\n");
-        /*tc_sub2();*/
-    }
-    CATCH(CHARLIE_EXCEPTION)
-    {
-        printf("catching CHARLIE_EXCEPTION: %s\n", e->msg);
-        TRY
-        {
-            printf("trying something else..\n");
-            THROW(CHARLIE_EXCEPTION, "something went bad with Charlie!");
-        }
-        CATCHALL
-        {
-            printf("gotta catch(%d) em all: %s (%s)\n", e->type, e->msg, e->previous ? e->previous->msg : "no rethrow");
-        }
-        ETRY;
-        printf("same catching CHARLIE_EXCEPTION: %s\n", e->msg);
-    }
-    FINALLY
-    {
-        printf("cleaning up!\n");
-    }
-    ETRY;
-
-    printf("\n");
-    printf("main continues, so all exceptions (if any) were caught!\n");
-
-    return 0;
-}
-
